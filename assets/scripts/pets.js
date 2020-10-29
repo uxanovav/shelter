d3d6e1d29c5e4dbbe4e741a56404b94211977444
uxanovav <uxanovav@yandex.ru>
2020-10-29 10:11:09 +0400

    feat: fix burger-menu  font size & height

--- assets/scripts/pets.js
+++ assets/scripts/pets.js
@@ -6,7 +6,7 @@ burgerButton.addEventListener('click', showBurgerMenu);
 
 function showBurgerMenu () {
     if (burgerToggler === false){
-        burgerMenu.style.height = '220px';
+        burgerMenu.style.height = '200px';
         burgerToggler = true;
     } else {
         burgerMenu.style.height = '0px';
